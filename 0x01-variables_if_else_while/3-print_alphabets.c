a5f74e0116475c9fee9a517e552a74cfcf7e89c2
Odun-boy <oduno73@gmail.com>
2022-07-01 23:29:56 -0700

    tasks

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,23 +2,22 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *main - Task 3 prints the alphabet in lowercase, and then in uppercase
- *followed by a new line
- *
- *Return: 0
- **/
+*main - Task 3 prints the alphabet in lowercase, and then in uppercase
+*followed by a new line
+*
+*Return: 0
+**/
 int main(void)
 {
-        int ch;
-
-        for (ch = 97; ch <= 122; ch++)
-        {
-        putchar(ch);
-        }
-        for (ch = 65; ch <= 90; ch++)
-        {
-        putchar(ch);
-        }
-        putchar(10); /* this is the ASCII code for new line*/
-        return (0);
+int ch;
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+for (ch = 65; ch <= 90; ch++)
+{
+putchar(ch);
+}
+putchar(10); /* this is the ASCII code for new line*/
+return (0);
 }

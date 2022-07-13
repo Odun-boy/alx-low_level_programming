1bc7fc2a25bc571ca35c9ecf38347816fe86183f
Odun-boy <oduno73@gmail.com>
2022-07-13 13:38:49 -0700

    Tasks

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,17 @@
 #include "main.h"
-
 /**
- * _puts - Write a function that prints a string, followed by a new line, to
- * stdout.
- *
- * @str: This is my input string
- *
- */
-
+* _puts - Write a function that prints a string, followed by a new line, to
+* stdout.
+*
+* @str: This is my input string
+*
+*/
 void _puts(char *str)
 {
-    int index;
-
-    for (index = 0; str[index] != '\0'; index++)
-    {
-        _putchar(str[index]);
-    }
-    _putchar('\n');
+int index;
+for (index = 0; str[index] != '\0'; index++)
+{
+_putchar(str[index]);
+}
+_putchar('\n');
 }

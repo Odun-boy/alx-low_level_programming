ff8ef25430c42f8be9f46f49a94ed612fa779681
Odun-boy <oduno73@gmail.com>
2022-08-14 02:08:11 -0700

    tasks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,22 +11,22 @@ list_t *add_node_end(list_t **head, const char *str)
 list_t *new, *temp;
 size_t nchar;
 new = malloc(sizeof(list_t));
-if (new == NULL)
+if(new == NULL)
 return (NULL);
 new->str = strdup(str);
-for (nchar = 0; str[nchar]; nchar++);
+for(nchar = 0; str[nchar]; nchar++);
 new->len = nchar;
 new->next = NULL;
 temp = *head;
-if (temp == NULL)
+if(temp == NULL)
 {
 *head = new;
 }
 else
 {
-while (temp->next != NULL)
+while(temp->next != NULL)
 temp = temp->next;
 temp->next = new;
 }
-return (*head);
+return(*head);
 }
